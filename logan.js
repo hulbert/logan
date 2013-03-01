ec89973eeac070e8e912348f570b9402953f4fb3
Scott Hulbert <scott@scotthulbert.com>
2013-03-01 15:02:35 -0800

    hacky fix for extra space for single digit day of month dates

--- logan.js
+++ logan.js
@@ -7,6 +7,8 @@
 		var logInfo = {};
 		var logLine = logLine.trim();
 		var logItems = logLine.split(' ');
+		
+		if (logItems[1] == '') logItems.splice(1,1);
 		if (logItems.length < 17) return {};
 	
 		logInfo.date = logItems.slice(0,3).join(' ');
